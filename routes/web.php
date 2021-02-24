c69a3f1069a201e218fd180a911a7abf4742b849
derekharget <git@derekharget.com>
2021-02-23 22:02:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,8 @@ $posts = [
     ]
 ];
 
-Route::resource('posts', PostsController::class)
-        ->only(['index', 'show', 'create', 'store', 'edit', 'update']);
+Route::resource('posts', PostsController::class);
+        // ->only(['index', 'show', 'create', 'store', 'edit', 'update']);
 
 // Route::get('/posts', function () use($posts) {
 //     // dd(request()->all());
