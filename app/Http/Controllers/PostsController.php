f8119f1c0f32da8b017531f722ac05aa04f976f0
derekharget <git@derekharget.com>
2021-02-27 04:13:30 -0500

    updated comment views

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -29,7 +29,8 @@ class PostsController extends Controller
         //     }
         // }
         // dd(DB::getQueryLog());
-        return view('posts.index',['posts' => BlogPost::all()]);
+        return view('posts.index',
+        ['posts' => BlogPost::withCount('comments')->get()]);
     }
 
     /**
